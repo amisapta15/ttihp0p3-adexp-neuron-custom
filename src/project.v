271ddce3956308bb5929f89b9b52de4bd0b594c6
Saptarshi Ghosh <sapta15@gmail.com>
2025-09-01 07:51:45 +0200

    First complete VHDl commit

--- src/project.v
+++ src/project.v
@@ -5,6 +5,9 @@
 
 `default_nettype none
 
+// Uncomment to switch back to LUT16
+//`define USE_LUT16
+
 module tt_um_dpi_adexp (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
@@ -16,12 +19,23 @@ module tt_um_dpi_adexp (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+`ifdef USE_LUT16
+    adex_neuron_system_tt_lut16 core (
+        .clk(clk), .rst_n(rst_n),
+        .ui_in(ui_in),
+        .uio_in(uio_in),
+        .uo_out(uo_out),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe)
+    );
+`else
+    adex_neuron_system_tt_lut32 core (
+        .clk(clk), .rst_n(rst_n),
+        .ui_in(ui_in),
+        .uio_in(uio_in),
+        .uo_out(uo_out),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe)
+    );
+`endif
 endmodule
